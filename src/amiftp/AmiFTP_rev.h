33716c6d18d4ef697b7804619cee2911adab64f5
GoosMcGuile <59646530+GoosMcGuile@users.noreply.github.com>
2022-08-23 18:16:13 +0200

    bumprev and changelog

--- src/amiftp/AmiFTP_rev.h
+++ src/amiftp/AmiFTP_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		1
-#define REVISION	951
-#define DATE		"17.2.2020"
-#define VERS		"AmiFTP 1.951"
-#define VSTRING		"AmiFTP 1.951 (17.2.2020)\r\n"
-#define VERSTAG		"\0$VER: AmiFTP 1.951 (17.2.2020)"
+#define REVISION	952
+#define DATE		"23.8.2022"
+#define VERS		"AmiFTP 1.952"
+#define VSTRING		"AmiFTP 1.952 (23.8.2022)\r\n"
+#define VERSTAG		"\0$VER: AmiFTP 1.952 (23.8.2022)"
